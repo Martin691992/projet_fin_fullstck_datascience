878241609a824dee4e0354f22864cd6c2e6f1da3
Martin BONNARD <bonnard.martin@gmail.com>
2025-06-28 10:54:37 +0200

    changement pour mysql

--- data_extraction/db_connection/db.py
+++ data_extraction/db_connection/db.py
@@ -1,11 +1,11 @@
 from dotenv import load_dotenv
-import psycopg
+
 import os
 
 class DbConnectorDataSet:
     def __init__(self):
         load_dotenv()
-        conn = psycopg.connect(
+        conn = sql.connect(
             dbname=os.getenv("DB_NAME"),
             user=os.getenv("DB_USER"),
             password=os.getenv("DB_PASSWORD"),
@@ -13,8 +13,18 @@ class DbConnectorDataSet:
             port=os.getenv("DB_PORT")
         )
         conn.execute("""
-        CREATE TABLE IF NOT EXISTS raw_data ()
-        """)
+        CREATE TABLE IF NOT EXISTS raw_data (id SERIAL PRIMARY KEY,
+                                            nom_raison_sociale TEXT,
+                                            nombre_etablissements_ouverts INTEGER,
+                                            activite_principale TEXT,
+                                            categorie_entreprise TEXT,
+                                            date_creation TEXT ,
+                                            date_fermeture TEXT,
+                                            etat_administratif TEXT,
+                                            nature_juridique TEXT,
+                                            section_activite_principale TEXT,
+                                            tranche_effectif_salarie TEXT,
+                                            departement VARCHAR(3))""")
         conn.commit()
 
 
--- data_extraction/extract.py
+++ data_extraction/extract.py
@@ -1,5 +1,19 @@
 import requests
+import time
+from db_connection.db import DbConnectorDataSet
 
 # On utilise ici l'API du gouvernement français afin de récupérer les données sur les entreprises.
 # La documentation est disponible via le lien ci-après : https://recherche-entreprises.api.gouv.fr
 #  
+
+# request = "https://recherche-entreprises.api.gouv.fr/search?page=1&per_page=25&est_association=false&est_collectivite_territoriale=false&est_service_public=false&est_l100_3=false"
+
+
+# response = requests.get(request)
+# results = response.json()
+# print(results["total_results"])
+# print(results["page"])
+# print(results["per_page"])
+# print(results["total_pages"])
+db = DbConnectorDataSet()
+
