5418315543fd17f18572612a64bc80d53ab7aede
Martin BONNARD <bonnard.martin@gmail.com>
2025-07-02 18:02:55 +0200

    travail 02/07

--- pipeline.py
+++ pipeline.py
@@ -1,7 +1,9 @@
+import pandas as pd
 
-with open('C:/Users/bonna/Downloads/StockUniteLegaleHistorique_utf8/StockUniteLegaleHistorique_utf8.csv', encoding='utf-8') as f:
-     for i in range(100):
-        ligne = f.readline()
-        if not ligne:
-            break  # fin du fichier atteinte avant 100 lignes
-        print(ligne.strip())
+chunksize = 100_000  # 100 000 lignes par bloc
+
+for index, chunk in enumerate(pd.read_csv('C:/Users/bonna/Downloads/StockUniteLegale_utf8/StockUniteLegale_utf8.csv', chunksize=chunksize)):
+    if index == 1 :
+        break
+    tri = chunk[['etatAdministratifUniteLegale','']]
+    print(chunk)
